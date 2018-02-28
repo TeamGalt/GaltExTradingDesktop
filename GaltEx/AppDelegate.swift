ada72c96fab50bee482db3daa10d73f78b0a289a
kuyawa <haxapp@gmail.com>
2018-02-28 13:11:02 -0400

    Initial Commit

---
+++ GaltEx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GaltEx
+//
+//  Created by Laptop on 2/28/18.
+//  Copyright © 2018 GaltBank. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GaltEx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GaltEx
+//
+//  Created by Laptop on 2/28/18.
+//  Copyright © 2018 GaltBank. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
